3bca167d5d3798b50ac297c4b785891300429082
rebootedCoder2 <oborimoon@gmail.com>
2024-10-23 12:16:40 +0900

    use navigate

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,5 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Router from "./Router";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 
 function Root() {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
+  const onHomeClick = () => {
+    navigate("/");
+  };
+
   return (
     <header>
       <ul>
         <li>
           <Link to={"/"}>Home</Link>
+          <button onClick={onHomeClick}>Home</button>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          {/* <Link to={"/about"}>About</Link> */}
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
