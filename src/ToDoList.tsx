4b16ebcf5700b8f53aa002cd9080bd806a972d7b
rebootedCoder2 <oborimoon@gmail.com>
2024-10-29 12:52:39 +0900

    form-custom validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -5,15 +5,27 @@ interface IForm {
   email: string;
   password: string;
   password1: string;
+  extraError?: string;
 }
 function ToDoList() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
-  } = useForm<IForm>();
-  const onValid = (data: any) => {
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
     console.log(data);
+    if (data.password !== data.password1) {
+      setError("password1", {
+        message: "password가 일치하지 않는다.",
+      });
+    }
+    setError("extraError", { message: "Server offline" });
   };
   console.log(errors);
   return (
@@ -25,11 +37,13 @@ function ToDoList() {
         <input
           {...register("username", {
             required: true,
-            minLength: {
-              value: 5,
-              message:
-                "Username 의 길이가 너무 짧다. 5글자 이상으로 해줘",
-            },
+            // minLength: {
+            //   value: 5,
+            //   message:
+            //     "Username 의 길이가 너무 짧다. 5글자 이상으로 해줘",
+            // },
+            validate: (value) =>
+              value.includes("nico") ? "nico 포함 안돼" : true,
           })}
           placeholder="Wite username"
         />
@@ -66,8 +80,9 @@ function ToDoList() {
           })}
           placeholder="Wite possword confirm"
         />
-        <span>{errors?.password?.message}</span>
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
