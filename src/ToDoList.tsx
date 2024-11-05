a67210cda2c0853abe58aeebe64a040a58f59780
rebootedCoder2 <oborimoon@gmail.com>
2024-11-05 21:05:34 +0900

    selector part1

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,19 +1,32 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./components/CreateToDo";
-import { toDoState } from "./components/atom";
+import { toDoSelcector, toDoState } from "./components/atom";
 import ToDo from "./components/ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
+  const [toDo, doing, done] = useRecoilValue(toDoSelcector);
   return (
     <div>
       <h1> ToDos</h1>
       <hr />
       <CreateToDo />
+      <h2> To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <h2> Doing</h2>
+      <ul>
+        {doing.map((doing) => (
+          <ToDo key={doing.id} {...doing} />
+        ))}
+      </ul>
+      <h2> Done</h2>
+      <ul>
+        {done.map((done) => (
+          <ToDo key={done.id} {...done} />
         ))}
       </ul>
     </div>
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,42 @@
-import { IToDo } from "./atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex(
+        (toDo) => toDo.id === id
+      );
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-
-      <button>Done</button>
-      <button>Done</button>
-      <button>Doing</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
     </li>
   );
 }
