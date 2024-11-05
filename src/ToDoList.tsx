893142d348454ef8582521b708823e57736253e4
rebootedCoder2 <oborimoon@gmail.com>
2024-11-05 21:14:15 +0900

    modify ui

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -11,18 +11,21 @@ function ToDoList() {
       <h1> ToDos</h1>
       <hr />
       <CreateToDo />
+      <hr />
       <h2> To Do</h2>
       <ul>
         {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
+      <hr />
       <h2> Doing</h2>
       <ul>
         {doing.map((doing) => (
           <ToDo key={doing.id} {...doing} />
         ))}
       </ul>
+      <hr />
       <h2> Done</h2>
       <ul>
         {done.map((done) => (
